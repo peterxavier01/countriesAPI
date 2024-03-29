be252d8c6504e5f882ea734ddc18632a61fbf964
Peter Uadiale <fxavy02@gmail.com>
2024-03-29 20:59:27 +0100

    refactor: add link to main text logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,9 @@ const Header = () => {
 
   return (
     <header>
-      <h1>Where in the world?</h1>
+      <h1>
+        <a href="/">Where in the world?</a>
+      </h1>
       <div onClick={handleThemeSelect}>
         {theme === "light" ? <BsMoon /> : <BsMoonFill />}
         <span>Dark Mode</span>
